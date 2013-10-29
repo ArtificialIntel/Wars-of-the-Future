85d75e4bac577834c86bd925149da1bd39430018
Sascha Eglau <sascha.eglau@gmail.com>
2013-10-29 14:10:43 +0800

    Added game setup screen

--- scripts/singleplayer.js
+++ scripts/singleplayer.js
@@ -11,12 +11,12 @@ var singleplayer = {
         $('.layer').hide();
         $('#menu').show();
     },
+    currentLevel:0,
 	startCurrentLevel:function(){
 	    // Load all the items for the level
 	    var level = maps.singleplayer[singleplayer.currentLevel];
 
-	    // Don't allow player to enter mission until all assets for the level are loaded
-	    $("#entermission").attr("disabled", true);
+	    $("#startGame").attr("disabled", true);
 
 	    // Load all the assets for the level
 	    game.currentMapImage = loader.loadImage(level.mapImage);
@@ -27,22 +27,19 @@ var singleplayer = {
 
 	    // Enable the enter mission button once all assets are loaded
 	    if (loader.loaded){
-	        $("#entermission").removeAttr("disabled");
+	        $("#startGame").removeAttr("disabled");
 	    } else {
 	        loader.onload = function(){
-	            $("#entermission").removeAttr("disabled");
+	            $("#startGame").removeAttr("disabled");
 	        }
 	    }
 
-	    // Load the mission screen with the current briefing
-	    $('#missonbriefing').html(level.briefing.replace('\n','<br><br>'));    
-	    $("#missionscreen").show();        
+	    $("#setupScreen").show();
 	},
-   
+
     play:function(){
-        game.animationLoop();                        
+        game.animationLoop();
         game.animationInterval = setInterval(game.animationLoop,game.animationTimeout);
-        game.start();
-    },   
-     
+        game.startGame();
+    },
 };
