ab895adec21a4644f0d40e8ad44b6b16ccb6ce3b
ianadmu <ianadmu@mackey.local>
2013-11-23 18:50:03 +0800

    Set player units to lower right corner by default

--- scripts/maps.js
+++ scripts/maps.js
@@ -17,9 +17,9 @@ var maps = {
 
             "items":[
                 // x and y are specified in # of squares on grid
-			    {"type":"staticUnits","name":"base","x":3,"y":4,"team":"A"},
-			    {"type":"dynamicUnits","name":"transport","x":10,"y":14,"team":"B","direction":"0"},
-			    {"type":"movableUnits","name":"chopper","x":8,"y":13,"team":"A","direction":"2"},
+			    {"type":"staticUnits","name":"base","x":15,"y":17,"team":"A"},
+			    {"type":"dynamicUnits","name":"transport","x":13,"y":17,"team":"B","direction":"0"},
+			    {"type":"movableUnits","name":"chopper","x":12,"y":17,"team":"A","direction":"2"},
 			    {"type":"terrain","name":"bigrocks","x":15,"y":15}
             ]
         },
