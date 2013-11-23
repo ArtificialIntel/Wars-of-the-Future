469b0bf29a1c668896b0fcf8f6835efc8de66c0e
Akhil Jain <hp@AKHIL.(none)>
2013-11-24 02:30:47 +0800

    unit placement changed

--- scripts/maps.js
+++ scripts/maps.js
@@ -20,9 +20,9 @@ var maps = {
 			    {"type":"staticUnits","name":"base","x":15,"y":17,"team":"A"},
 			    {"type":"dynamicUnits","name":"transport","x":13,"y":17,"team":"A","direction":"0"},
 			    {"type":"movableUnits","name":"chopper","x":12,"y":17,"team":"A","direction":"2"},
-			    {"type":"staticUnits","name":"base","x":1,"y":1,"team":"B"},
-			    {"type":"dynamicUnits","name":"transport","x":10,"y":10,"team":"B","direction":"0"},
-			    {"type":"movableUnits","name":"chopper","x":8,"y":10,"team":"B","direction":"2"},
+			    {"type":"staticUnits","name":"base","x":Math.random(5),"y":Math.random(5),"team":"B"},
+			    {"type":"dynamicUnits","name":"transport","x":10,"y":7,"team":"B","direction":"0"},
+			    {"type":"movableUnits","name":"chopper","x":5,"y":12,"team":"B","direction":"2"},
 			    {"type":"terrain","name":"bigrocks","x":15,"y":15}
             ]
         },
